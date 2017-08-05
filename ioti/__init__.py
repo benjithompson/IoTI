054027187efa7498805b69f40500631dc16d1a2f
benjithompson <mail@benjithompson.com>
2017-08-04 22:02:43 -0500

    initial file setup

---
+++ ioti/__init__.py
@@ -0,0 +1,2 @@
+"""init"""
+from .ioti import app
---
+++ ioti/ioti.py
@@ -0,0 +1,12 @@
+"""Flask API"""
+
+#imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__, instance_path='C:/Code/Python/IoTI/ioti/instance')
+
+app.config.from_object(config.ini)
+
