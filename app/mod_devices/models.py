9c9b489a40807cd27d358f7d5e0eb2223f5c5613
benjithompson <mail@benjithompson.com>
2017-09-17 22:17:26 -0500

    adjust db fields

--- app/mod_devices/models.py
+++ app/mod_devices/models.py
@@ -7,20 +7,21 @@ class Device(db.Model):
     __tablename__ = 'device_devices'
 
     id         = db.Column(db.Integer, primary_key=True)
-    ipaddress  = db.Column(db.String, unique=True)
+    ip         = db.Column(db.String, unique=False)
     name       = db.Column(db.String(30), unique=False)
-    desc       = db.Column(db.String(120), unique=False)
+    category   = db.Column(db.String(120), unique=False)
     date_added = db.Column(db.String(20), default=db.func.current_timestamp())
-    owner      = db.Column(db.String(20), unique=False)
+    owner      = db.Column(db.String(20), db.ForeignKey('auth_user.username'))
     datas      = db.relationship('Data', backref='device_devices', lazy='dynamic')
 
-    def __init__(self, ipaddress, name=None, desc=None):
-        self.ipaddress = ipaddress
-        self.name      = name
-        self.desc      = desc
+    def __init__(self, ip, owner=owner, name=None, category=None):
+        self.ip       = ip
+        self.name     = name
+        self.category = category
+        self.owner    = owner
 
     def __repr__(self):
-        return '<Device: ipaddress={0.ipaddress!r}, name={0.name!r}, desc={0.desc!r}, date_added={0.date_added!r}>'.format(self)
+        return '<Device: ipaddress={0.ipaddress!r}, name={0.name!r}, category={0.category!r}, date_added={0.date_added!r}>'.format(self)
 
 class Data(db.Model):
 
